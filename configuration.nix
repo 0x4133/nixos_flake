cdde0381f7ec589adcc122b90cc487308d0dab26
aaron <0x4133@gmail.com>
2025-07-27 21:54:21 -0700

    chore: update flakes on 2025-07-27 21:54:21

--- configuration.nix
+++ configuration.nix
@@ -160,12 +160,7 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  services.hyprpaper = {
-     enable = true;
-     settings = {
-      # Your hyprpaper settings here
-     };
-   };
+
 
 
   # List packages installed in system profile. To search, run:
@@ -173,6 +168,7 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+     hyprpaper
      dmenu
     
   ];
