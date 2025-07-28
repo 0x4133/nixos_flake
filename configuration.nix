41779a0aac4806d951f2d27f9c80deefed70fbf5
aaron <0x4133@gmail.com>
2025-07-27 21:52:39 -0700

    chore: update flakes on 2025-07-27 21:52:39

--- configuration.nix
+++ configuration.nix
@@ -131,7 +131,7 @@
        wofi
      # dolphin
        firefox
-       dmenu
+       
        bash
        micro
        fastfetch
@@ -160,12 +160,21 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  services.hyprpaper = {
+     enable = true;
+     settings = {
+      # Your hyprpaper settings here
+     };
+   };
+
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
-     hyprpaper
+     dmenu
+    
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
