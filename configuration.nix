a4c5df62fee74ea1f9c3da473f1665253b5f8bfe
aaron <0x4133@gmail.com>
2025-08-18 12:21:02 -0700

    chore: update flakes on 2025-08-18 12:21:02

--- configuration.nix
+++ configuration.nix
@@ -215,7 +215,7 @@ services.udev.packages = [ pkgs.hackrf ];
         gqrx
         hackrf
         sdrangel
-        vmware
+        
     ];
   };
 networking.firewall.trustedInterfaces = [ "incusbr0" ];
