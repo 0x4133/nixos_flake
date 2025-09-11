a532d318529909f1e1b3ec281c343b8b54a8de94
aaron <0x4133@gmail.com>
2025-09-11 10:04:19 -0700

    chore: update flakes on 2025-09-11 10:04:19

--- configuration.nix
+++ configuration.nix
@@ -246,6 +246,7 @@ services.udev.packages = [ pkgs.hackrf ];
         xorg.libXinerama
         xorg.libXi
         xorg. libXxf86vm
+        wf-recorder
        # Sectools
         nmap masscan rustscan amass subfinder nuclei fierce dnsenum
          theharvester responder netexec enum4linux-ng nikto
