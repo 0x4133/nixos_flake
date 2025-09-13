1ef79631d7b284cba4e9e7728d76c63a5a37787a
aaron <0x4133@gmail.com>
2025-09-13 07:31:34 -0700

    chore: update flakes on 2025-09-13 07:31:34

--- configuration.nix
+++ configuration.nix
@@ -248,7 +248,8 @@ services.udev.packages = [ pkgs.hackrf ];
         xorg.libXi
         xorg. libXxf86vm
         wf-recorder
-        nordvpn
+        nord
+       
        # Sectools
         nmap masscan rustscan amass subfinder nuclei fierce dnsenum
          theharvester responder netexec enum4linux-ng nikto
