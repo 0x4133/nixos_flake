41826eed28fec11597e998966a5af323088184e6
aaron <0x4133@gmail.com>
2025-09-21 10:05:47 -0700

    chore: update flakes on 2025-09-21 10:05:47

--- configuration.nix
+++ configuration.nix
@@ -326,7 +326,7 @@ environment.systemPackages = with pkgs; [
 ];
 
  environment.sessionVariables = {
-    NIX_PROFILES = "${yabridge-pkg}";
+    NIX_PROFILES = "/nix/store/mgm684yazy2rz7c7nflrjxckdzvg9hah-yabridge-5.1.1/lib/libyabridge-chainloader-vst2.so";
     # Replace this with the actual path to your VST directories
     YABRIDGE_PLUGIN_DIRS = "/home/aaron/.wine/drive_c/Program Files/Common Files/VST3";
   };
