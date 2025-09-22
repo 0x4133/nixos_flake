5a859718451554c0d2cfd0e8a3206ae77153ca71
aaron <0x4133@gmail.com>
2025-09-21 19:07:47 -0700

    chore: update flakes on 2025-09-21 19:07:47

--- configuration.nix
+++ configuration.nix
@@ -252,7 +252,7 @@ services.udev.packages = [ pkgs.hackrf ];
         yabridge
         yabridgectl
         carla
-
+        sox
         
        # Sectools
         nmap masscan rustscan amass subfinder nuclei fierce dnsenum
