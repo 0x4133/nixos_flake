50040c55fb0da02781bb693fb5ddcdd120645bbf
aaron <0x4133@gmail.com>
2025-10-09 08:27:37 -0700

    chore: update flakes on 2025-10-09 08:27:37

--- flake.nix
+++ flake.nix
@@ -1,20 +1,36 @@
+# flake.nix
 {
-  description = "A very basic flake (nixos-unstable)";
+  description = "A very basic flake (nixos-unstable) with winapps";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Add winapps and make it follow our nixpkgs
+    winapps = {
+      url = "github:winapps-org/winapps";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, winapps, ... }:
   let
     # Change if you're on a different arch:
     system = "x86_64-linux";
   in {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       inherit system;
-      specialArgs = { inherit nixpkgs self; }; # plus anything else you want to pass
+      specialArgs = { inherit nixpkgs self; };
+
       modules = [
         ./configuration.nix
+
+        # Add winapps packages to your system
+        ({ pkgs, ... }: {
+          environment.systemPackages = [
+            winapps.packages.${system}.winapps
+            winapps.packages.${system}.winapps-launcher # optional
+          ];
+        })
       ];
     };
   };
